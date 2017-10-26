7a3f7dfb7b87ba11eafa84b8763f204ced65e4ef
tdn90 <nguyentridung1212@gmail.com>
2017-10-26 17:34:37 -0400

    Added Methods to LiveObjects

--- Facebuk/src/LiveObject.java
+++ Facebuk/src/LiveObject.java
@@ -36,4 +36,25 @@ class LiveObject extends FacebukObject {
 	Moment getOverallHappiestMoment () {
 		return null;
 	}
+	
+	/*
+	 * !!!
+	 * Need to implement
+	 * return a list containing the maximum clique of friends with whom the target person/pet could go out, 
+	 * such that each of his/her friends is also friends with everyone else in the set.
+	 */
+	ArrayList findMaximumCliqueOfFriends () {
+		return null;
+	}
+	
+	/*
+	 * !!!
+	 * Need to implement
+	 * returns true if and only if all the people/pets in the specified set are all friends with each other. 
+	 * Since this is a helper method that should not depend on any instance variables of the target object, it can (and should) be declared static. 
+	 * Even though this is a helper method, make sure it is declared public so we can test it!
+	 */
+	static boolean isClique (ArrayList set) {
+		return true;
+	}
 }
