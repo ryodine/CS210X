80f01c5091e344c8c8fe1a23230cf5e96bf4df47
alescion <33096482+alescion@users.noreply.github.com>
2017-11-15 21:58:39 -0500

    Added Javadocs

--- Project3_IMDB/src/IMDBActorsGraph.java
+++ Project3_IMDB/src/IMDBActorsGraph.java
@@ -6,11 +6,20 @@ public class IMDBActorsGraph extends IMDBGraph{
 	public IMDBActorsGraph (String actorsFileName, String actressesFilename) throws IOException {
 		super(actorsFileName, actressesFilename);
 	}
-	
+
+	/**
+	 *
+	 * @return a Collection of ActorNodes provided by the superclass IMDBGraph
+	 */
 	public Collection<? extends Node> getNodes (){
 		return actormap.values();
 	}
-	
+
+	/**
+	 *
+	 * @param name the name of the requested Node
+	 * @return the ActorNode associated with that name
+	 */
 	public Node getNodeByName (String name){
 		return actormap.get(name);
 	}
