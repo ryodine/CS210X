75c95fec6f549db2d7f9f5de1b976f0200ba1ce8
Ryan Johnson <ryan@johnsonweb.us>
2017-11-28 16:40:07 -0500

    RNGesus

--- Project5/src/ExpressionGenerator.java
+++ Project5/src/ExpressionGenerator.java
@@ -6,9 +6,37 @@ public class ExpressionGenerator {
 
     public static void main(String[] args) {
         System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+        System.out.println(createExpression());
+
     }
 
     public static String createExpression() {
+        Double random = rng.nextDouble();
+        if (random < 0.1) {
+            return "x";
+        } else if (random > 0.85) {
+            return "x + " + createExpression();
+        } else if (random > 0.7) {
+            return "x * " + createExpression();
+        } else if (random > 0.55) {
+            return createExpression() + " * x";
+        } else if (random > 0.4) {
+            return createExpression() + " + x";
+        } else {
+            return "(" + createExpression() + ")";
+        }
 
     }
 }
