548999c50b0ee0d931595413686d7ba68489bf63
Ryan Johnson <ryan@johnsonweb.us>
2017-11-28 16:17:38 -0500

    start of 5

---
+++ Project5/src/ExpressionGenerator.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class ExpressionGenerator {
+
+    private static final Random rng = new Random();
+
+    public static void main(String[] args) {
+        System.out.println(createExpression());
+    }
+
+    public static String createExpression() {
+
+    }
+}
