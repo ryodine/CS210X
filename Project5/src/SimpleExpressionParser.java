91118dfaebdb7f149d5416a1de6f4eef44255c9c
tdn90 <nguyentridung1212@gmail.com>
2017-12-06 17:02:57 -0500

    95% done - Tested / Added Javadoc
    May need to add some Javadoc for SimpleCompoundExpression
    
    PartialTester2 is a clone for me to make sure the tests can run. You guys should check it again using PartialTester1.
    
    tester is used to find bugs and debugs.

--- Project5/src/SimpleExpressionParser.java
+++ Project5/src/SimpleExpressionParser.java
@@ -2,7 +2,8 @@ import java.lang.reflect.Parameter;
 import java.util.function.Function;
 
 /**
- * Starter code to implement an ExpressionParser. Your parser methods should use the following grammar: (updated)
+ * Completed code for SimpleExpressionParser. 
+ * Parse expression into a tree according to the following grammar:
  * E := E+M | m
  * M := M*X | X
  * X := (E) | L
@@ -30,13 +31,25 @@ public class SimpleExpressionParser implements ExpressionParser {
 		return expression;
 	}
 	
-	protected Expression parseExpression (String str) {
+	/**
+	 * Main helper method for parse
+	 * Parse an expression into a binary expression tree
+	 * @param str: String input
+	 * @return the top Expression after parsing the String input 
+	 */
+	private Expression parseExpression (String str) {
 		Expression expr = parseE(str);
-
 		return expr;
 	}
 
-
+	/**
+	 * Abstract method for parsing E and M
+	 * @param str: String input
+	 * @param operator: operator input (should be either '+' or '*')
+	 * @param parseFn1: function1 input 
+	 * @param parseFn2: function2 input
+	 * @return an Expression tree given a specified String input
+	 */
 	private static Expression parserHelper (String str, char operator, Function<String, Expression> parseFn1, Function<String, Expression> parseFn2) {
 		AbstractCompoundExpression expr;
 		switch (operator) {
@@ -58,29 +71,42 @@ public class SimpleExpressionParser implements ExpressionParser {
 			if (resultFn1 != null && resultFn2 != null) {
 				expr.addSubexpression(resultFn1);
 				expr.addSubexpression(resultFn2);
-
 				return expr;
 			}
-
-			idxOfOp = str.indexOf('+', idxOfOp+1);
+			idxOfOp = str.indexOf(operator, idxOfOp+1);
 		}
 
-		//Alternate case
+		// Alternate case
 		Expression altResult = parseFn2.apply(str);
 		return altResult;
 	}
 
-	//Addition
+	/**
+	 * Helper method for parseExpression 
+	 * Parse E -> E+M | M
+	 * @param str: String input
+	 * @return an Expression at the top of the expression tree parsed from the String input
+	 */
 	private static Expression parseE(String str) {
 		return parserHelper(str, '+', SimpleExpressionParser::parseE, SimpleExpressionParser::parseM);
 	}
 
-	//Multiplicative
+	/**
+	 * Helper method for parseExpression 
+	 * Parse M -> M*X | X
+	 * @param str: String input
+	 * @return an Expression at the top of the expression tree parsed from the String input
+	 */
 	private static Expression parseM(String str) {
 		return parserHelper(str, '*', SimpleExpressionParser::parseM, SimpleExpressionParser::parseX);
 	}
 
-	//Parens
+	/**
+	 * Helper method for parseExpression 
+	 * Parse X -> (E) | L
+	 * @param str: String input
+	 * @return an Expression at the top of the expression tree parsed from the String input
+	 */
 	private static Expression parseX(String str) {
 		if (str.length() >= 3 && str.substring(0,1).equals("(") && str.substring(str.length()-1, str.length()).equals(")")) {
 
@@ -99,7 +125,13 @@ public class SimpleExpressionParser implements ExpressionParser {
 		return altsub;
 	}
 
-	//Literal
+	/**
+	 * Parser helper for LiteralExpression
+	 * Parse L -> [0-9]+ | [a-z]
+	 * @param str: String input
+	 * @return a LiteralExpression for the given String 
+	 * @return null if given String is not valid
+	 */
 	private static Expression parseL(String str) {
 		if (isLetter(str) || isNumber(str)) {
 			return new LiteralExpression(str);
@@ -109,6 +141,11 @@ public class SimpleExpressionParser implements ExpressionParser {
 		}
 	}
 	
+	/**
+	 * Helper method for parseL (Literal Expression)
+	 * @param str: String input 
+	 * @return true if it is a valid letter, false otherwise
+	 */
 	private static boolean isLetter(String str) {
 		String name = str.toLowerCase();
 		int length = str.length();
@@ -120,9 +157,15 @@ public class SimpleExpressionParser implements ExpressionParser {
 				return false;
 			}
 		}
-		return (str.length() > 0);
+		return (str.length() == 1);
 	}
 	
+	/**
+	 * Helper method for parseL (Literal Expression)
+	 * @param str: String input
+	 * @return true if the String input is a valid number, false otherwise
+	 * Valid number conditions: has at least one digit, each character is from 0 to 9. 
+	 */
 	private static boolean isNumber(String str){
 		String check = "0123456789";
 		int length = str.length();
--- Project5/src/tester.java
+++ Project5/src/tester.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
+import java.util.*;
+
 public class tester {
+	static Scanner user = new Scanner(System.in);
 	private static boolean isLetter(String str) {
 		String name = str.toLowerCase();
 		int length = str.length();
@@ -12,7 +15,7 @@ public class tester {
 				return false;
 			}
 		}
-		return true;
+		return (str.length() == 1);
 	}
 	
 	private static boolean isNumber(String str){
@@ -26,13 +29,19 @@ public class tester {
 				return false;
 			}
 		}
-		return true;
+		return (str.length() > 0);
 	}
 	
 	public static void main (String[] agrs) {
+		
 		// return true
 		
+		
+		// Is pre-zeroes-number considered valid? 
+		// 
+		
 		// Assume that 0000001 is valid
+		/*
 		ArrayList<String> getRight = new ArrayList<String>();
 		getRight.add("a");
 		getRight.add("b");
@@ -45,6 +54,9 @@ public class tester {
 		getRight.add("145238745");
 		
 		System.out.println("This is all true: ");
+		for (String a : getRight) {
+			System.out.println(isLetter(a) || isNumber(a));
+		}
 		
 		
 		System.out.println();
@@ -61,7 +73,121 @@ public class tester {
 		getWrong.add("7*");
 		getWrong.add("7)");
 		getWrong.add("8953228reg.,");
+		getWrong.add("");
+		for (String a : getWrong) {
+			System.out.println(isLetter(a) || isNumber(a));
+		}
+		
+		System.out.println(isLetter("adf") || isNumber("adf"));
+		*/
+		
+		
+		SimpleExpressionParser parser = new SimpleExpressionParser();
+		/*
+		for (int i = 0; i < 20; i ++) {
+			try {
+				System.out.println("Enter new expression: ");
+				String input = user.nextLine();
+				input = input.replaceAll(" ", "");
+				Expression current = parser.parse(input, false);
+				System.out.println("---------------------");
+				System.out.println(current.convertToString(0));
+				System.out.println("---------------------");
+			}
+			
+			catch (Exception e) {
+				System.out.println("Not valid Expression. Enter again:");
+				e.printStackTrace();
+				continue;
+			}
+			
+		}
+		*/
+		
+		
+		/*
+		System.out.println("Enter new expression: ");
+		String input = user.nextLine();
+		input = input.replaceAll(" ", "");
+		Expression current = parser.parseExpression(input);
+		System.out.println(current.convertToString(0));
+		*/
+		
+		
+		//2*x + 3*y + (7+6*z)
+		
+		Expression two = new LiteralExpression("2");
+		Expression x = new LiteralExpression("x");
+		Expression three = new LiteralExpression("3");
+		Expression y = new LiteralExpression("y");
+		Expression seven = new LiteralExpression("7");
+		Expression six = new LiteralExpression("6");
+		Expression z = new LiteralExpression("z");
+		
+		MultiplicativeExpression multi2x = new MultiplicativeExpression();
+		multi2x.addSubexpression(two);
+		multi2x.addSubexpression(x);
+		
+		MultiplicativeExpression multi3y = new MultiplicativeExpression();
+		multi3y.addSubexpression(three);
+		multi3y.addSubexpression(y);
+		
+		MultiplicativeExpression multi6z = new MultiplicativeExpression();
+		multi6z.addSubexpression(six);
+		multi6z.addSubexpression(z);
+		
+		AdditiveExpression plus7Sub6z = new AdditiveExpression();
+		plus7Sub6z.addSubexpression(seven);
+		plus7Sub6z.addSubexpression(multi6z);
+		
+		AdditiveExpression plus2xAnd3y = new AdditiveExpression();
+		plus2xAnd3y.addSubexpression(multi2x);
+		plus2xAnd3y.addSubexpression(multi3y);
+		
+		ParentheticalExpression paren = new ParentheticalExpression();
+		paren.addSubexpression(plus7Sub6z);
+		
+		AdditiveExpression top = new AdditiveExpression();
+		top.addSubexpression(plus2xAnd3y);
+		top.addSubexpression(paren);
+		
+		System.out.println(top.convertToString(0));
+		
+		top.flatten();
+		System.out.println(top.convertToString(0));
+		
+		
+		
+		
+		
+		System.out.println("---------------------");
+		System.out.println("Enter new expression: ");
+		String input = user.nextLine();
+		input = input.replaceAll(" ", "");
+		//Expression current = parser.parseExpression(input);
+		//System.out.println(current.convertToString(0));
+		//current.flatten();
+		//System.out.println(current.convertToString(0));
+		
+		
+		/*
+		ParentheticalExpression top1 = new ParentheticalExpression();
+		
+		ParentheticalExpression top2 = new ParentheticalExpression();
+		top1.addSubexpression(top2);
+		ParentheticalExpression top3 = new ParentheticalExpression();
+		top2.addSubexpression(top3);
+		ParentheticalExpression top4 = new ParentheticalExpression();
+		top3.addSubexpression(top4);
+		top4.addSubexpression(six);
+		
+		System.out.println(top1.convertToString(0));
+		
+		*/
+		
 		
 		
 	}
+	
+	
 }
